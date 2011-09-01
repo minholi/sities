aaf6f1f3d66ca597c321c29056854f5097559c7b
Marcelo Minholi <minholi@gmail.com>
2011-08-31 21:36:37 -0300

    Added taxonomy for palestras.

--- functions.php
+++ functions.php
@@ -23,10 +23,10 @@ function create_sities_palestra() {
                 'add_new_item' => __( 'Adicionar Nova Palestra' ),
                 'edit_item' => __( 'Editar Palestra' )
             ),
-        'public' => true,
-        'has_archive' => true,
-        'rewrite' => array('slug' => 'palestras', 'with_front' => False),
-        'menu_position' => 5
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => array('slug' => 'palestras', 'with_front' => False),
+            'menu_position' => 5
         )
     );
 }
@@ -46,10 +46,10 @@ function create_sities_palestrante() {
                 'add_new_item' => __( 'Adicionar Novo Palestrante' ),
                 'edit_item' => __( 'Editar Palestrante' )
             ),
-        'public' => true,
-        'has_archive' => true,
-        'rewrite' => array('slug' => 'palestrantes', 'with_front' => False),
-        'menu_position' => 5
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => array('slug' => 'palestrantes', 'with_front' => False),
+            'menu_position' => 5
         )
     );
 }
@@ -58,14 +58,47 @@ function create_sities_palestrante() {
 /**
  * Cria uma ligação entre Palestra e Palestrante
  */
+add_action( 'init', 'sities_connection_types', 100 );
+
 function sities_connection_types() {
     if ( !function_exists( 'p2p_register_connection_type' ) )
         return;
 
-    p2p_register_connection_type( array( 
-        'from' => 'sities_palestra',
-        'to' => 'sities_palestrante'
-    ) );
+    p2p_register_connection_type( 
+        array( 
+            'from' => 'sities_palestra',
+            'to' => 'sities_palestrante'
+        )
+    );
 }
-add_action( 'init', 'sities_connection_types', 100 );
+
+
+/**
+ * Cria taxonomia para classificação dos tipos de palestras 
+ * (mini cursos, palestras, mesa redonda, etc.)
+ */
+add_action( 'init', 'create_sities_taxonomies', 0 );
+
+function create_sities_taxonomies() {
+    register_taxonomy('sities_palestra_tipo','sities_palestra',
+        array(
+            'hierarchical' => True,
+            'labels' => array(
+                'name' => __( 'Tipos' ),
+                'singular_name' => __( 'Tipo' ),
+                'parent_item' => null,
+                'parent_item_colon' => null,
+                'edit_item' => __( 'Editar Tipo' ), 
+                'update_item' => __( 'Atualizar Tipo' ),
+                'add_new_item' => __( 'Adicionar Novo Tipo' ),
+                'new_item_name' => __( 'Novo Tipo' ),
+                'separate_items_with_commas' => __( 'Separar tipos com vírgulas' ),
+                'add_or_remove_items' => __( 'Adicionar ou remover tipos' ),
+                'choose_from_most_used' => __( 'Escolher a partir dos tipos mais usados' ),
+                'menu_name' => __( 'Tipos' ),
+            ),
+        )
+    );
+}
+
 ?>

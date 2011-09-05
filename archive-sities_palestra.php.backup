bd232b5e8da463c4b05b31beeaf77edd9a961a8e
Marcelo Minholi <minholi@gmail.com>
2011-09-05 12:03:03 -0300

    Templates de noticias e pagina inicial

---
+++ archive-sities_palestra.php.backup
@@ -0,0 +1,58 @@
+<?php
+/**
+ * The template for displaying Archive pages.
+ *
+ * Used to display archive-type pages if nothing more specific matches a query.
+ * For example, puts together date-based pages if no date.php file exists.
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Sities
+ * @since Sities 0.3.0
+ */
+
+get_header(); ?>
+
+		<section id="primary">
+			<div id="content" role="main">
+
+			<?php if ( have_posts() ) : ?>
+
+				<header class="page-header">
+					<h1 class="page-title">Programação</h1>
+				</header>
+
+				<?php twentyeleven_content_nav( 'nav-above' ); ?>
+
+                <?php $query = new WP_Query( array( 'meta_key' => 'data', 'meta_value' => '06/10' ) ); ?>
+                
+                <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+                    <li>
+                    <?php the_title(); ?>
+                    </li>
+                <?php endwhile; ?>
+
+                <?php wp_reset_postdata(); ?>
+                
+				<?php twentyeleven_content_nav( 'nav-below' ); ?>
+
+			<?php else : ?>
+
+				<article id="post-0" class="post no-results not-found">
+					<header class="entry-header">
+						<h1 class="entry-title"><?php _e( 'Nothing Found', 'twentyeleven' ); ?></h1>
+					</header><!-- .entry-header -->
+
+					<div class="entry-content">
+						<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'twentyeleven' ); ?></p>
+						<?php get_search_form(); ?>
+					</div><!-- .entry-content -->
+				</article><!-- #post-0 -->
+
+			<?php endif; ?>
+
+			</div><!-- #content -->
+		</section><!-- #primary -->
+
+<?php get_footer(); ?>
---
+++ front-page.php
@@ -0,0 +1,52 @@
+﻿<?php
+/**
+ * Template da pagina inicial
+ *
+ * @package WordPress
+ * @subpackage Sities
+ * @since Sities 0.4.0
+ */
+
+get_header(); ?>
+
+		<div id="primary">
+			<div id="content" role="main">
+
+                <?php include (ABSPATH . '/wp-content/plugins/featured-content-gallery/gallery.php'); ?>
+
+                <div id="colunas">
+                <div class="coluna" id="coluna1">
+                    <a class="item-coluna" id="inscricoes" href="#">
+                        <h2>Inscrições</h2>
+                        <p>Se deseja participar do Sities, efetue sua inscrição aqui. A inscrição
+                        prévia não tem custo e ao informar seus dados você estará concorrendo a
+                        brindes de nossos patrocinadores.</p>
+                    </a>
+                    <a class="item-coluna" id="localizacao" href="#">
+                        <h2>Localização</h2>
+                        <p>O Sities será realizado no campus do IFPR, na rodovia PR-323 próximo ao
+                        trevo de acesso de Mariluz (sentido Guaíra). O local conta com amplo
+                        estacionamento próprio.</p>
+                    </a>
+                </div>
+
+                <div class="coluna" id="coluna2">
+                    <a class="item-coluna" id="palestras" href="#">
+                        <h2>Programação</h2>
+                        <p>Veja aqui as principais atrações do Sities, os horários em que serão
+                        ministradas as palestras, mini-cursos e demais atividades. Lembre-se
+                        de montar seu programa pessoal.</p>
+                    </a>
+                    <a class="item-coluna" id="palestrantes" href="#">
+                        <h2>Palestrantes</h2>
+                        <p>Saiba quem estará ministrando palestras e mini-cursos no Sities.
+                        Conheça um pouco do perfil dos palestrantes e veja quem tem o perfil
+                        mais próximo ao que você procura.</p>
+                    </a>
+                </div>
+                </div>
+
+			</div><!-- #content -->
+		</div><!-- #primary -->
+
+<?php get_footer(); ?>
